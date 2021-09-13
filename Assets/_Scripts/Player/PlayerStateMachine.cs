cdc81f3e7bcc97f692455f7d7801ce49be9c7c99
SuperNewPC\SuperPC <khaustov.maksym@gmail.com>
2021-09-13 15:28:10 +0300

    main class now switches states (doing transition)

--- Assets/_Scripts/Player/PlayerStateMachine.cs
+++ Assets/_Scripts/Player/PlayerStateMachine.cs
@@ -2,17 +2,50 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody), typeof(Animator))]
 public class PlayerStateMachine : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private PlayerState _firstState;
+
+    private PlayerState _currentState;
+    private Rigidbody _rigidbody;
+    private Animator _animator;
+
+
+    private void Awake()
     {
-        
+        _rigidbody = GetComponent<Rigidbody>();
+        _animator = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
-    void Update()
+
+    private void Start()
     {
+        _currentState = _firstState;
+        _currentState.Enter(_rigidbody, _animator);
+
+    }
+
+
+    private void Update()
+    {
+        if (_currentState != null) return;
+
+        PlayerState nextState = _currentState.GetNextState();
+        if (nextState != null)
+        {
+            Transit(nextState);
+        }
+    }
+
+
+    private void Transit(PlayerState nextState)
+    {
+        if (_currentState != null) { _currentState.Exit(); }
+
+        _currentState = nextState;
+
+        if (_currentState != null) { _currentState.Enter(_rigidbody, _animator); }
         
     }
 }
