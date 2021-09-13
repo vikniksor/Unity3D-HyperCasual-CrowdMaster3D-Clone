e8b3aef8097934e9c59301ab9c00992aa039fb8b
SuperNewPC\SuperPC <khaustov.maksym@gmail.com>
2021-09-13 20:53:59 +0300

    fix typo in name

--- Assets/_Scripts/Player/Transitions/AttackTransition.cs
+++ Assets/_Scripts/Player/Transitions/AttackTransition.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AtackTransition : PlayerTransition
+public class AttackTransition : PlayerTransition
 {
     public override void Enable()
     {
