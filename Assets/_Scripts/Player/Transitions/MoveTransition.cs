2a34c5b17315b907bdfb9b0b49df86112797d50a
SuperNewPC\SuperPC <khaustov.maksym@gmail.com>
2021-09-13 18:15:38 +0300

    add move transition so state machine now can switch from idle to move

---
+++ Assets/_Scripts/Player/Transitions/MoveTransition.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveTransition : PlayerTransition
+{
+    public override void Enable()
+    {
+        
+    }
+
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0)) { NeedTransit = true; }
+    }
+}
