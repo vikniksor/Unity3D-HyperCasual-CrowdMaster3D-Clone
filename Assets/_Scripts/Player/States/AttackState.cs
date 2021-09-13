2583fdbefbe36ab9375c1acaab421bb775255fef
SuperNewPC\SuperPC <khaustov.maksym@gmail.com>
2021-09-13 20:38:36 +0300

    add a scriptable object for attack

---
+++ Assets/_Scripts/Player/States/AttackState.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackState : PlayerState
+{
+    [SerializeField] private StaminaAccumulator _staminaAccumulator;
+}
