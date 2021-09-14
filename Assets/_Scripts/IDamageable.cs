42ec1cd557ce5435e87c434cf59c02bb7dc15757
SuperNewPC\SuperPC <khaustov.maksym@gmail.com>
2021-09-14 13:03:44 +0300

    fix typo in name of script apply changes to prefab

--- Assets/_Scripts/IDamageable.cs
+++ Assets/_Scripts/IDamageable.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public interface IDamagable
+public interface IDamageable
 {
     bool ApplyDamage(Rigidbody rigidbody, float force);
 }
